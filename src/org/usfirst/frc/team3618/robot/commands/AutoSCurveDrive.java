f3cf1c1330318b48964810d5a52efdfa71b44f27
Wayne Ross <mr_trekkie@yahoo.com>
2018-03-08 01:22:48 -0500

    S-Curve driving changes

--- src/org/usfirst/frc/team3618/robot/commands/AutoSCurveDrive.java
+++ src/org/usfirst/frc/team3618/robot/commands/AutoSCurveDrive.java
@@ -6,53 +6,49 @@ import org.usfirst.frc.team3618.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
-
 /**
  *
  */
-public class AutoDriveCommand extends Command {
+public class AutoSCurveDrive extends Command {
 	double goal;
 	double minimum;
-	double inchGoal;
-	double goalAngle;
 	double maxTime;
 	static final double SCALER = 48000.0;
+	double remaining;
+	double curve;
 	
-    public AutoDriveCommand(double inches, double angle) {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+
+    public AutoSCurveDrive(double inches, boolean rightSide) {
     	requires(Robot.kDriveSubsystem);
-    	inchGoal = inches;
-    	goalAngle = angle;
-    	goal = inches * DriveSubsystem.ENCODER_COUNTS_PER_INCH;
-    	maxTime = 5.0 / (220 * DriveSubsystem.ENCODER_COUNTS_PER_INCH) * goal;
+    	remaining = goal = inches * DriveSubsystem.ENCODER_COUNTS_PER_INCH;
+    	maxTime = 8.0 / (220 * DriveSubsystem.ENCODER_COUNTS_PER_INCH) * goal;
     	minimum = 0.38;
+    	curve = 0.7; // not more than 1; how agressive is the curve we turn (how far sideways will we go?)
+    	if (!rightSide)
+    		curve *= -1; // left is opposite sign
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-//    	Robot.kDriveSubsystem.resetRobotAngle();
     	Robot.kDriveSubsystem.encoderReset();
+    	Robot.kDriveSubsystem.resetRobotAngle(); // need to always start with zero when doing S-Curve
     	Robot.kDriveSubsystem.shifToHighGear(false);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	double remaining;
-    	remaining = goal - Robot.kDriveSubsystem.getLeftCounts();
+    	remaining = goal - Robot.kDriveSubsystem.getAverageCounts();
     	double power = remaining / SCALER;
     	if(power < minimum) {
     		power = minimum;
     	}
-    	SmartDashboard.putNumber("Inches", Robot.kDriveSubsystem.getLeftCounts() / DriveSubsystem.ENCODER_COUNTS_PER_INCH);
-   		Robot.kDriveSubsystem.driveStraightGyro(power, goalAngle); 
-    	
+    	SmartDashboard.putNumber("Inches", Robot.kDriveSubsystem.getAverageCounts() / DriveSubsystem.ENCODER_COUNTS_PER_INCH);
+   		Robot.kDriveSubsystem.driveSCurve(power,goal-remaining,goal,curve); 
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (Robot.kDriveSubsystem.getLeftCounts() / DriveSubsystem.ENCODER_COUNTS_PER_INCH > inchGoal) || (maxTime > timeSinceInitialized());
+        return (remaining / DriveSubsystem.ENCODER_COUNTS_PER_INCH <= 0) || (maxTime > timeSinceInitialized());
     }
 
     // Called once after isFinished returns true
